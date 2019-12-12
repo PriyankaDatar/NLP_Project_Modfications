e9a061bbc69de7682d57e01b2468275025891c7d
Priyanka Datar <priyankadatar2013@gmail.com>
2019-12-12 18:11:09 -0500

    modified

--- test.py
+++ test.py
@@ -75,9 +75,9 @@ if __name__ == '__main__':
 
     print("Dev acc_qm: %s;\n  breakdown on (agg, sel, where): %s"%epoch_acc(
             model, BATCH_SIZE, val_sql_data, val_table_data, TEST_ENTRY))
-    # print("Dev execution acc: %s"%epoch_exec_acc(
-    #         model, BATCH_SIZE, val_sql_data, val_table_data, DEV_DB))
+    print("Dev execution acc: %s"%epoch_exec_acc(
+             model, BATCH_SIZE, val_sql_data, val_table_data, DEV_DB))
     print("Test acc_qm: %s;\n  breakdown on (agg, sel, where): %s"%epoch_acc(
             model, BATCH_SIZE, test_sql_data, test_table_data, TEST_ENTRY))
-    # print("Test execution acc: %s"%epoch_exec_acc(
-    #         model, BATCH_SIZE, test_sql_data, test_table_data, TEST_DB))
+    print("Test execution acc: %s"%epoch_exec_acc(
+             model, BATCH_SIZE, test_sql_data, test_table_data, TEST_DB))
